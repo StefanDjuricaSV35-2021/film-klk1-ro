1b483630c7e3e15b78ff3a2b57b69bf80e6c96bc
Stefan Djurica <104830728+StefanDjuricaSV35-2021@users.noreply.github.com>
2024-04-14 19:01:51 +0200

    added start

---
+++ film-application/src/test/java/com/example/filmapplication/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
